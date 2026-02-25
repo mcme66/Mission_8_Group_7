19f41f1914c5a82862d7d86d720a83abac53f4ee
meme66 <allenschultz05@gmail.com>
2026-02-25 07:56:12 -0700

    Add project files.

---
+++ Mission_8_Group_7/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Mission_8_Group_7.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
