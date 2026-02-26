fe0cecf81fab4a3396732b7774682d0dd1ac0095
camjwebb <camjwebb8@gmail.com>
2026-02-26 09:26:21 -0700

    adfs

--- Mission_8_Group_7/Controllers/HomeController.cs
+++ Mission_8_Group_7/Controllers/HomeController.cs
@@ -1,25 +1,96 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Mission_8_Group_7.Models;
-using System.Diagnostics;
+using Mission_8_Group_7.Repositories;
 
 namespace Mission_8_Group_7.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private ITaskRepository _repo;
+        public HomeController(ITaskRepository repo)
         {
-            return View();
+            _repo = repo;
         }
 
-        public IActionResult Privacy()
+        public IActionResult Quadrants()
         {
-            return View();
+            var tasks = _repo.GetTasks();
+            return View(tasks);
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
+        [HttpGet]
+        public IActionResult AddEdit(int? id)
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            ViewBag.Categories = new SelectList(_repo.GetCategories(), "CategoryId", "CategoryName");
+            if (id.HasValue)
+            {
+                var task = _repo.GetTaskById(id.Value);
+                if (task == null) return NotFound();
+                return View("AddEdit", task);
+            }
+            return View("AddEdit", new TaskItem());
+        }
+
+        [HttpPost]
+        public IActionResult AddEdit(TaskItem variable)
+        {
+            if (ModelState.IsValid)
+            {
+                if (variable.TaskItemId == 0)
+                {
+                    _repo.AddTask(variable);
+                }
+                else
+                {
+                    _repo.UpdateTask(variable);
+                }
+                _repo.Save();
+                return RedirectToAction("Quadrants");
+            }
+            ViewBag.Categories = new SelectList(_repo.GetCategories(), "CategoryId", "CategoryName");
+            return View("AddEdit", variable);
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            var task = _repo.GetTaskById(id);
+            if (task == null) return NotFound();
+            return View(task);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var task = _repo.GetTaskById(id);
+            if (task != null)
+            {
+                _repo.DeleteTask(task);
+                _repo.Save();
+            }
+            return RedirectToAction("Quadrants");
+        }
+
+        [HttpGet]
+        public IActionResult MarkComplete(int id)
+        {
+            var task = _repo.GetTaskById(id);
+            if (task == null) return NotFound();
+            return View(task);
+        }
+
+        [HttpPost, ActionName("MarkComplete")]
+        public IActionResult MarkCompleteConfirmed(int id)
+        {
+            var task = _repo.GetTaskById(id);
+            if (task != null)
+            {
+                task.Completed = true;
+                _repo.UpdateTask(task);
+                _repo.Save();
+            }
+            return RedirectToAction("Quadrants");
         }
     }
 }
---
+++ Mission_8_Group_7/Repositories/ITaskRepository.cs
@@ -0,0 +1,14 @@
+﻿using Mission_8_Group_7.Models;
+namespace Mission_8_Group_7.Repositories
+{
+    public interface ITaskRepository
+    {
+        List<TaskItem> GetTasks();
+        List<Category> GetCategories();
+        TaskItem GetTaskById(int id);
+        void AddTask(TaskItem task);
+        void UpdateTask(TaskItem task);
+        void DeleteTask(TaskItem task);
+        void Save();
+    }
+}
